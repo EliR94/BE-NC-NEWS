fab4d8de691f5e8db2ad68b2c216013380beb136
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2024-05-28 16:26:59 +0100

    sets up /api/articles/:article_id endpoint in app.js with TDD, MVC and error handling. refactors /api/topics and /api to ensure they return the data in an object. updates endpoints.json adding /api/articles/:article_id key:value

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -3,7 +3,7 @@ const { fetchEndpoints } = require("../models/api.models");
 exports.getEndpoints = (req, res, next) => {
     return fetchEndpoints()
         .then((endpoints) => {
-            res.status(200).send(endpoints)
+            res.status(200).send({ endpoints })
         })
         .catch((err) => next(err));
 }
