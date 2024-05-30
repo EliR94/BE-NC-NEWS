05f13a19dc268d595c3f77ca2405342e60d7812b
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2024-05-30 15:26:05 +0100

    adds DELETE /api/comments/:comment_id with TDD, MVC and error handling and refactors

--- models/comments.models.js
+++ models/comments.models.js
@@ -16,3 +16,23 @@ exports.insertCommentByArticleId = (article_id, newComment) => {
             return rows[0]
         })
 }
+
+exports.fetchCommentByCommentId = (comment_id) => {
+    return db
+    .query("SELECT * FROM comments WHERE comment_id = $1", [comment_id])
+    .then(({rows}) => {
+        const comment = rows[0];
+        if(!rows.length){
+            return Promise.reject({
+                status: 404,
+                msg: `No comment found for comment id: ${comment_id}`
+            })
+        }
+        return comment;
+    })
+}
+
+exports.removeCommentById = (comment_id) => {
+    return db
+    .query("DELETE FROM comments WHERE comment_id = $1", [comment_id])
+}
