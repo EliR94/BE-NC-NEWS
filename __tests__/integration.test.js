1e9de81d90248b78f2bf497f9d342476cc7e1cb2
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2024-05-30 08:45:23 +0100

    adds /api/articles/:article_id/comments and refactors /api/articles

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -79,7 +79,6 @@ describe('GET /api/articles/:article_id', () => {
             expect(body.msg).toBe("Bad Request")
         })
     });
-    // 
 });
 describe('GET /api/articles', () => {
     test('status 200: returns an array of article objects each of which with the following properties: author, title, article_id, topic, created_at, votes, article_img_url, comment_count', () => {
@@ -88,7 +87,8 @@ describe('GET /api/articles', () => {
         .expect(200)
         .then(({body}) => {
             const articles = body.articles
-            expect(articles).toBeSorted({ descensing: true })
+            expect(articles).toHaveLength(13)
+            expect(articles).toBeSorted({ descending: true })
             for(const property in articles){
                 expect(articles[property]).not.toContainKey("body")
                 expect(articles[property]).toMatchObject({
@@ -102,6 +102,45 @@ describe('GET /api/articles', () => {
                     comment_count: expect.any(Number)
                 })
             }
+        })
+    });
+});
+describe('GET /api/articles/:article_id/comments', () => {
+    test('status 200: should return array of comments for the given article_id, each comment with the following properties: comment_id, votes, created_at, author, body, article_id, ordered by date descending', () => {
+        return request(app)
+        .get("/api/articles/9/comments")
+        .expect(200)
+        .then(({body}) => {
+            const comments = body.comments
+            expect(comments).toHaveLength(2)
+            expect(comments).toBeSortedBy('created_at',{descending: true})
+            comments.forEach((comment) => {
+                expect(comment).toMatchObject({
+                    comment_id: expect.any(Number),
+                    votes: expect.any(Number),
+                    created_at: expect.any(String),
+                    author: expect.any(String),
+                    body: expect.any(String),
+                    article_id: expect.any(Number)
+                })
+            })
+        })
+    });
+    test('status 200: should return empty array when given an article_id with no comments', () => {
+        return request(app)
+        .get("/api/articles/2/comments")
+        .expect(200)
+        .then(({body}) => {
+            const comments = body.comments
+            expect(comments).toHaveLength(0)
+        })
+    });
+    test('status 404: should return error message when given an article_id which doesnt exist in the articles table', () => {
+        return request(app)
+        .get("/api/articles/999999999/comments")
+        .expect(404)
+        .then(({body}) => {
+            expect(body.msg).toBe("Article Not Found")
         })
     });
 });
--- models/comments.models.js
+++ models/comments.models.js
@@ -1,13 +1,9 @@
 const db = require("../db/connection.js")
 
-exports.countComments = (article_id) => {
+exports.fetchCommentsByArticleId = (article_id) => {
     return db
-        .query("SELECT * FROM comments WHERE article_id = $1", [article_id])
+        .query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC", [article_id])
         .then(({rows}) => {
-            if(!rows.length){
-                return 0
-            } else {
-                return rows.length
-            }
+            return rows
         })
 }
